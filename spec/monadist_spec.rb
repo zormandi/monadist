c5dcf7e6538a4c671ec61011cfa90ea779b4ba85
Zoltan Ormandi <zoltan.ormandi@emarsys.com>
2014-11-18 21:52:42 +0100

    Bumped version to 0.1

--- spec/monadist_spec.rb
+++ spec/monadist_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe Monadist do
   it 'should have a version number' do
-    expect(Monadist::VERSION).to eq '0.0.1'
+    expect(Monadist::VERSION).to eq '0.1'
   end
 end
